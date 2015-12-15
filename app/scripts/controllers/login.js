8787a614f5141f0d3ddf92f72c10fcf4a1f736f3
James Byrne <jamesdesbyrne@gmail.com>
2015-12-15 22:36:26 +0000

    Finished the main page & added the login page

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name richWeb2App.controller:LoginCrtl
+ * @description
+ * # LoginCtrl
+ * Controller of the richWeb2App
+ * This controller handles user login 
+ */
+angular.module('richWeb2App')
+
+
+
+.controller('LoginCtrl', function ($scope) {
+
+  // Set the background image
+  $scope.style = {
+    "background-color" : "F3F3F3",
+    "min-height" : "100%"
+  };
+
+});
